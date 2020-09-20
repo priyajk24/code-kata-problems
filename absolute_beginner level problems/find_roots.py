ccd0a09b605fad2511213069c817789102218917
priyajk24 <51525572+priyajk24@users.noreply.github.com>
2020-09-20 13:29:33 +0530

    Update find_roots.py

--- absolute_beginner level problems/find_roots.py
+++ absolute_beginner level problems/find_roots.py
@@ -2,8 +2,9 @@ import math
 A,B,C = input().split()
 A,B,C = [int(i) for i in [A,B,C]]
 
-x = (-B) + (math.sqrt((B**2)-(4*A*C)))/ 2*A
-x1 = (-B) - (math.sqrt((B**2)-(4*A*C)))/ 2*A
+sq = math.sqrt((B**2) - 4*A*C)
+x = (-B + sq) / (2*A)
+x1 = (-B - sq) / (2*A)
 
 print(round(x,2))
 print(round(x1,2))
